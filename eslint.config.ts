a2fedba74df96fcee5567d902245491312860dfa
Bitsulov <roma.bitsulov@mail.ru>
2026-01-08 02:49:48 +0300

    chore: update project configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,55 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default [
-    { ignores: ["dist"] },
+export default defineConfig([
+    globalIgnores(['dist']),
     {
-        // Теперь обрабатываем JS, JSX, TS и TSX
-        files: ["**/*.{js,jsx,ts,tsx}"],
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
-                ecmaFeatures: { jsx: true }
-            },
-            globals: globals.browser
-        },
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactRefresh.configs.vite,
+        ],
         plugins: {
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-            "@typescript-eslint": tsPlugin,
-            react: reactPlugin,
-            prettier: prettierPlugin
+            'react-hooks': reactHooks
         },
-        rules: {
-            // Сохраняем старые JS правила
-            ...js.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-
-            "key-spacing": ["error", { beforeColon: false, afterColon: true }],
-
-            // TypeScript правила (по желанию)
-            "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-
-            // React Refresh
-            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-
-            // Prettier интеграция
-            "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-
-            // Отключаем правила, которые могут конфликтовать с Prettier
-            ...prettierConfig.rules
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
-        settings: {
-            react: { version: "detect" }
+        "rules": {
+            "eol-last": ["error", "always"]
         }
-    }
-];
+    },
+])
