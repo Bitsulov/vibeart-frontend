61ff957b017a5830a1b224f0e0b6df44cdcf4aca
Bitsulov <roma.bitsulov@mail.ru>
2026-03-08 22:55:25 +0300

    add homeReviews widget

---
+++ src/widgets/homeReviews/config/reviewsImgConfig.ts
@@ -0,0 +1,7 @@
+import reviewsImgRu from "shared/icons/img-reviews-ru.png";
+import reviewsImgEn from "shared/icons/img-reviews-en.png";
+
+export const reviewsImgConfig: Record<string, string> = {
+    "ru": reviewsImgRu,
+    "en": reviewsImgEn
+}
