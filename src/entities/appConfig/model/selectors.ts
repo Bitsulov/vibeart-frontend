d6447b90cea1025ca5badcbe6d4db7ecfd71a35c
Bitsulov <roma.bitsulov@mail.ru>
2026-03-29 01:25:19 +0300

    moved copyButton to component, suplement appConfig
    Copy button was taken out in invidial component.
    
    in appConfig add properties:
    - unreadChatsCount
    - unreadNotificationsCount

--- src/entities/appConfig/model/selectors.ts
+++ src/entities/appConfig/model/selectors.ts
@@ -2,3 +2,5 @@ import type {RootState} from "app/store";
 
 export const selectCurrentLanguage = (state: RootState) => state.app.currentLanguage;
 export const selectServerStatus = (state: RootState) => state.app.serverStatus;
+export const selectUnreadChatsCount = (state: RootState) => state.app.unreadChatsCount;
+export const selectUnreadNotificationsCount = (state: RootState) => state.app.unreadNotificationsCount;
